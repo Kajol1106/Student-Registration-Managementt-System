357a9d7f42438179e5ca48fb2b2c19dfcad5263c
Kajol1106 <kolagirkajol@gmail.com>
2022-11-12 18:02:35 +0530

    day-3 changes

---
+++ Exception/BatchException.java
@@ -0,0 +1,16 @@
+package com.Exception;
+
+public class BatchException extends Exception{
+
+	public BatchException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BatchException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
---
+++ Exception/InputMistMatchException.java
@@ -0,0 +1,16 @@
+package com.Exception;
+
+public class InputMistMatchException extends Exception {
+
+	public InputMistMatchException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InputMistMatchException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
